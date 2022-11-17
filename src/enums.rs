7c99d1216be73c929c1da51e2015fb5538cbad20
Kevin Heavey <ID+username@users.noreply.github.com>
2022-11-17 23:53:31 +0000

    start adding pyo3

--- src/enums.rs
+++ src/enums.rs
@@ -1,4 +1,6 @@
 use borsh::{BorshDeserialize, BorshSerialize};
+#[cfg(feature = "pyo3")]
+use pyo3::prelude::*;
 
 #[derive(BorshDeserialize, BorshSerialize, Copy, Clone, PartialEq, Eq, Debug)]
 pub enum SelfTradeBehavior {
@@ -7,12 +9,23 @@ pub enum SelfTradeBehavior {
     DecrementTake,
 }
 
+#[cfg_attr(feature = "pyo3", pyclass)]
 #[derive(BorshDeserialize, BorshSerialize, Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Side {
     Bid,
     Ask,
 }
 
+impl Side {
+    pub fn from_order_sequence_number(order_id: u64) -> Self {
+        match order_id.leading_zeros() {
+            0 => Side::Bid,
+            _ => Side::Ask,
+        }
+    }
+}
+
+#[cfg_attr(feature = "pyo3", pymethods)]
 impl Side {
     pub fn opposite(&self) -> Self {
         match *self {
@@ -21,10 +34,11 @@ impl Side {
         }
     }
 
-    pub fn from_order_sequence_number(order_id: u64) -> Self {
-        match order_id.leading_zeros() {
-            0 => Side::Bid,
-            _ => Side::Ask,
-        }
+    // cfg_attr doesn't work for staticmethod yet
+    #[cfg(feature = "pyo3")]
+    #[pyo3(name = "from_order_sequence_number")]
+    #[staticmethod]
+    pub fn py_from_order_sequence_number(order_id: u64) -> Self {
+        Self::from_order_sequence_number(order_id)
     }
 }
