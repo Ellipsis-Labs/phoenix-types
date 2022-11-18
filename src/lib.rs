1cc8fa8bc40d9308d5622ea866a63ed8b79c379f
Jarry Xiao <jarry.xiao@gmail.com>
2022-11-18 15:19:19 -0600

    hotfix program id

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub mod instructions;
 pub mod market;
 pub mod order_packet;
 
-solana_sdk::declare_id!("phnxuLaYrJdynas8LQu7eL9dVjCYEA6sFdCV5DuoPB6");
+solana_sdk::declare_id!("phnxNHfGNVjpVVuHkceK3MgwZ1bW25ijfWACKhVFbBH");
